f73564480b32a714b83f237f818d8b899200852c
princeryadav <prince.yadavp098@gmail.com>
2017-04-26 19:37:20 +0530

    added advJava

---
+++ AdvJava/src/com/bridgelabz/advJava/loginServlet.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.advJava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/loginServlet")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		PrintWriter printWriter=response.getWriter();
+		
+		String userName=request.getParameter("userName");
+		String passWord=request.getParameter("password");
+		
+		printWriter.println("Welcome "+userName);
+		
+		
+		
+	}
+
+	
+	
+}
